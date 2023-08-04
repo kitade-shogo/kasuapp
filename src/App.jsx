88fcc022daa7704163b1fa668a8caebb8e9a4ee0
kitadeshogo <tori3227@gmail.com>
2023-08-04 17:51:25 +0900

    デプロイ

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WhitespaceInterpreter from "./whiteSpaceInterpreter";
+
+function App() {
+  return (
+    <>
+      <WhitespaceInterpreter className="mx-auto"/>
+    </>
+  );
+}
+
+export default App;
